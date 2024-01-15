fa487dd051b4403061efec4bb7f61d3509cce1c7
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-01-15 23:08:55 +0100

    Create esercizio6.c

---
+++ esercizio6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    double popolazioneIniziale, popolazioneDaRaggiungere;
+    int anni = 0;
+
+    // Input della popolazione iniziale e quella da raggiungere
+    printf("Inserisci la popolazione iniziale di alghe: ");
+    scanf("%f", &popolazioneIniziale);
+
+    printf("Inserisci la popolazione da raggiungere o superare: ");
+    scanf("%f", &popolazioneDaRaggiungere);
+
+    // Calcolo del numero di anni necessari
+    while (popolazioneIniziale < popolazioneDaRaggiungere) {
+        popolazioneIniziale *= 2;   // Raddoppia la popolazione ogni anno
+        popolazioneIniziale /= 3;   // Cala di un terzo l'anno successivo
+        anni++;
+    }
+
+    // Output del risultato
+    printf("La popolazione raggiunge o supera il valore desiderato dopo %d anni.\n", anni);
+
+    return 0;
+}
