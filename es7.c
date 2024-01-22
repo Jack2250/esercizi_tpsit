25c19803160e5c3eaf4a5de91978ad84b2ed0f29
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-01-22 22:09:21 +0100

    Create es7.c

---
+++ es7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, numero, massimo, minimo;
+
+    printf("Quanti numeri vuoi inserire? ");
+    scanf("%d", &n);
+
+    // Chiedi all'utente di inserire i numeri
+    printf("Inserisci %d numeri:\n", n);
+
+    // Leggi il primo numero e inizializza massimo e minimo
+    scanf("%d", &numero);
+    massimo = minimo = numero;
+
+    // Leggi gli altri numeri e aggiorna massimo e minimo
+    for (int i = 1; i < n; ++i) {
+        scanf("%d", &numero);
+
+        if (numero > massimo) {
+            massimo = numero;
+        }
+
+        if (numero < minimo) {
+            minimo = numero;
+        }
+    }
+
+    // Stampare il risultato
+    printf("Il massimo numero inserito è: %d\n", massimo);
+    printf("Il minimo numero inserito è: %d\n", minimo);
+
+    return 0;
+}
