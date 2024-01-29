d36e7b062a46477df74d0b68836b0fc3f9b3968e
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-01-29 22:57:51 +0100

    Update esercizio8.c

--- esercizio8.c
+++ esercizio8.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     float soldi;
-    int pastina=1;
-    float panino = 1.5;
-    int scelta;
-    int giorno;
-    
-    printf("Inserisci soldi");
-    scanf("%f",&soldi);
-    
-    
-    while(soldi>0){
-        printf("Cosa vuoi mangiare \n 1 panino \n 2 pastina");
-        scanf("%d"&scelta);
-        if(scelta=1){
-            panino++;
-        }else if(scelta=2){
-            pastina++;
-        }else{
-            printf("scelta non valida");
+    int pastine = 0, panini = 0, giorni = 0;
+    char scelta;
+
+    printf("Inserisci la quantita' di soldi di Mario: ");
+    scanf("%f", &soldi);
+
+    while (1) {
+        printf("Mario vuole mangiare una pastina (P) o un panino (B)? (P/B): ");
+        scanf(" %c", &scelta);
+
+        if (scelta == 'P') {
+            if (soldi >= 1) {
+                pastine++;
+                soldi -= 1;
+            } else {
+                break;
+            }
+        } else if (scelta == 'B') {
+            if (soldi >= 1.5) {
+                panini++;
+                soldi -= 1.5;
+            } else {
+                break;
+            }
+        } else {
+            printf("Scelta non valida. Riprova.\n");
+            continue;
         }
-        
-        giorno++
+
+        giorni++;
     }
-    
-    printf("Mario ha mangiato %d\n giorni",giorno);
-    printf("Mario ha mangiato %d\n pastine",pastina);
-    printf("Mario ha mangiato %f\n panini",panino);
+
+    printf("Mario ha mangiato per %d giorni.\n", giorni);
+    printf("Ha mangiato %d pastine e %d panini.\n", pastine, panini);
 
     return 0;
 }
